615fac64a2d5d5450c40af1fb6890980eab65200
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2021-09-28 11:13:50 +0500

    Initial express setup

--- server.js
+++ server.js
@@ -10,4 +10,5 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
