af1ba738ca8d8dd979dbd96c617edf99b7c986d1
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2021-09-29 09:42:51 +0500

    ы

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// Route files
+const bootcamp = require('./routes/bootcamp');
 
 
 // Load env files
 dotenv.config({
     path: './config/config.env'
 });
-
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamp);
+
 const PORT = process.env.PORT || 5000;
-
-
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
