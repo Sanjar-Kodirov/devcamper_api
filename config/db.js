34faa3e217c4bba3dbe3b88a0e7f0e068350b9bf
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-08-21 22:13:57 +0500

    Connected to database mongodb atlas (initial setup ).

---
+++ config/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log(conn.connection.host);
+};
+module.exports = connectDB;
