3b7f01beee43bee2812ed0eceed98db2dfb3e127
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-08-22 14:16:39 +0500

    Bootcamp model is completed

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(conn.connection.host);
+  console.log(conn.connection.host.cyan.underline.bold);
 };
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db");
 const app = express();
 
@@ -19,7 +20,7 @@ const bootcamps = require("./routes/bootcamps");
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 const server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+  console.log(`Server is running on port ${port}.`.white.bgGreen.bold);
 });
 // handle unhandled rejections
 process.on("unhandledRejection", (err, promise) => {
