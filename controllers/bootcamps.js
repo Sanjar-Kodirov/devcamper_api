4391936189d09d7412c310da48a6bcd42bf3d7f2
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-08-29 11:45:21 +0500

    Mongoose error handling 2  (custom error handler completed)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -37,7 +37,7 @@ exports.getBootcamp = async (req, res, next) => {
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
-exports.createBootcamp = async (req, res) => {
+exports.createBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.create(req.body);
     res.status(200).json({
--- middleware/error.js
+++ middleware/error.js
@@ -6,11 +6,22 @@ function errorHandler(err, req, res, next) {
   error.message = err.message;
 
   // Log to console for dev
-  console.log(err);
+  console.log(err.errors);
 
   if (err.name === "CastError") {
     const message = `Resource not found`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duvplicate fiels entered`;
+    error = new ErrorResponse(message, 400);
+  }
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
