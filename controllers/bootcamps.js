8e5fd9aca890d53b7d29d87f038e16705f6b4525
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-09-02 03:47:07 +0500

    Geospatial Query - Get Bootcamps Within Radius completed

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -68,3 +68,30 @@ exports.deleteBootcamp = asyncHandler(async (req, res) => {
     data: bootcamp,
   });
 });
+
+// @desc      Get bootcamps within a radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res) => {
+  const { zipcode, distance } = req.params;
+  console.log(zipcode, distance);
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  // Earth Radius = 3,963 mi / 6,378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
