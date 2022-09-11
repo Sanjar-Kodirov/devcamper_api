d9c5fc71f349d356d9da8de3f105c3d2ef7fe831
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-09-12 02:48:11 +0500

    Aggregate - Calculating The Average Course Cost

--- seeder.js
+++ seeder.js
@@ -36,7 +36,7 @@ const courses = JSON.parse(
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    await Course.create(courses);
+    // await Course.create(courses);
     // await User.create(users);
     // await Review.create(reviews);
     console.log("Data Imported...".green.inverse);
