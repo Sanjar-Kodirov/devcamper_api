9c2d8bff7ae39e1358cfa52bb06e044c8c63d1e5
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-09-13 15:08:57 +0500

    sign jwt basic

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,28 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: true, data: user });
+  // create jwt token
+  const token = user.getSignedJwtToken();
 
+  res.status(200).json({ success: true, token, data: user });
   //   sendTokenResponse(user, 200, res);
+});
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 });
--- models/User.js
+++ models/User.js
@@ -41,4 +41,11 @@ UserSchema.pre("save", async function (req, res) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Sign user JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
 module.exports = mongoose.model("User", UserSchema);
